199ab853157738ca72db4e5cbd8ca9f73a3ed7f7
hemillainysantos <hemillainy.santos@ccc.ufcg.edu.br>
2018-10-04 14:17:53 -0300

    add product-controller

--- src/app.js
+++ src/app.js
@@ -1,36 +1,22 @@
 'use strict'
 
+const express = require('express');
 const bodyParser = require('body-parser');
 
-const express = require('express');
 const app = express();
-
-const index = require('./routes/index');
 const router = express.Router();
 
+// Carrega as Rotas
+const indexRoute = require('./routes/index-route');
+const productRoute = require('./routes/product-route');
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+// não codificar o url
+app.use(bodyParser.urlencoded({ 
+    extended: false
+}));
 
-
-const create = router.post('/', (req, res, next) => {
-    res.status(201).send(req.body);
-});
-
-const put = router.put('/:id', (req, res, next) => {
-    const id = req.params.id;
-    res.status(200).send({
-        id: id,
-        item: req.body
-    });
-});
-
-const del = router.delete('/', (req, res, next) => {
-    res.status(200).send(req.body);
-});
-
-app.use('/', index);
-app.use('/products', create);
-app.use('/products', put);
-app.use('/products', del);
+app.use('/', indexRoute);
+app.use('/products', productRoute);
 
 module.exports = app;
